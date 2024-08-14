57eaaa89063a978624d3fcdb9a67d45136ee7b8d
El-Praise Ayo <elpraise20@gmail.com>
2024-08-14 09:38:09 +0100

    chore: Update npm dependencies
    Update `azle` dependency to version `^0.19.0` and add `uuid` dependency with version `^10.0.0` in `package-lock.json` and `package.json` files.

--- src/typescript_tut_backend/src/index.ts
+++ src/typescript_tut_backend/src/index.ts
@@ -1,7 +1,87 @@
-import { Canister, query, text } from 'azle';
+import {
+  Canister,
+  query,
+  text,
+  nat64,
+  Record,
+  Result,
+  Principal,
+  Vec,
+  update,
+  StableBTreeMap,
+  nat8,
+  None,
+  ic,
+} from "azle";
+import { v4 as uuidv4 } from "uuid";
+// Define the EscrowStatus enum
+enum EscrowStatus {
+  PENDING = "PENDING",
+  RELEASED = "RELEASED",
+  DISPUTED = "DISPUTED",
+  CANCELLED = "CANCELLED",
+}
+
+// Define the Escrow type
+type Escrow = Vec<{
+  id: text;
+  seller: Principal;
+  buyer: Principal;
+  arbitrator: Principal;
+  amount: nat64;
+  status: EscrowStatus;
+  releaseTime: nat64;
+  description: text;
+}>;
+
+let escrowStorage = StableBTreeMap<text, Escrow>(0);
 
 export default Canister({
-    greet: query([text], text, (name) => {
-        return `Hello, ${name}!`;
-    })
-})
+  greet: query([text], text, (name) => {
+    return `Hello, ${name}!`;
+  }),
+
+  getEscrow: query([text], Result(text, Escrow), (id) => {
+    const escrow = escrowStorage.get(id);
+    if ("None" in escrow) {
+      return Result.Err("Escrow not found");
+    }
+    return Result.Ok(escrow.Some);
+  }),
+
+  createEscrow: update(
+    [Principal, Principal, Principal, nat64, text, nat64],
+    text,
+    (seller, buyer, arbitrator, amount, description, releaseTime) => {
+      const escrow = {
+        id: uuidv4(),
+        seller,
+        buyer,
+        arbitrator,
+        amount,
+        status: EscrowStatus.PENDING,
+        releaseTime,
+        description,
+      };
+      escrowStorage.insert(escrow.id, escrow);
+      return escrow.toString();
+    }
+  ),
+
+  releaseEscrow: query([text], Result(text, text), (id) => {
+    const escrow = escrowStorage.get(id);
+    if ("None" in escrow) {
+      return Result.Err("Escrow not found");
+    }
+    const currentEscrow = escrow.Some;
+    if (currentEscrow.status !== EscrowStatus.PENDING) {
+      return Result.Ok("Escrow is not pending");
+    }
+    if (currentEscrow.releaseTime < ic.time()) {
+      return Result.Ok("Release time has not been reached");
+    }
+    const updatedEscrow = { ...currentEscrow, status: EscrowStatus.RELEASED };
+    escrowStorage.insert(id, updatedEscrow);
+    return Result.Ok("Escrow released");
+  }),
+});
